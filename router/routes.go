62b3c2749527afd884a5c1643c89f88031429481
Soler <kaue.s.valeve@gmail.com>
2024-04-28 17:48:46 -0300

    Adjusted unit: routes.go description: created four more methods

--- router/routes.go
+++ router/routes.go
@@ -1,12 +1,34 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func InitializeRouter(router *gin.Engine) {
 	// Definition
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	version1 := router.Group("/api/version1")
+	{
+		version1.GET("/GET", func(structure *gin.Context) {
+			structure.JSON(http.StatusOK, gin.H{
+				"message": "Success GET",
+			})
 		})
-	})
+		version1.POST("/POST", func(structure *gin.Context) {
+			structure.JSON(http.StatusOK, gin.H{
+				"message": "Success POST",
+			})
+		})
+		version1.DELETE("/DELETE", func(structure *gin.Context) {
+			structure.JSON(http.StatusOK, gin.H{
+				"message": "Success DELETE",
+			})
+		})
+		version1.PUT("/PUT", func(structure *gin.Context) {
+			structure.JSON(http.StatusOK, gin.H{
+				"message": "Success PUT",
+			})
+		})
+	}
 }
