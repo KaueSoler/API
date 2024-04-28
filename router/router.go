a8804b8dc1da9bf96ba0a42e6d97901fd12792d4
Soler <kaue.s.valeve@gmail.com>
2024-04-28 16:44:52 -0300

    Separation of router.Initialize

--- router/router.go
+++ router/router.go
@@ -2,16 +2,10 @@ package router
 
 import "github.com/gin-gonic/gin"
 
-func Inicialize() {
-	// inicialização do router utilizando configurações padrão do gin-gonic
+func Initialize() {
+	// Initialize router
 	router := gin.Default()
 
-	// deifnição de uma rota
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 	// Api rodando, teste em postman http://localhost:8080/ping
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
